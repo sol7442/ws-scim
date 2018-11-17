92a32de4bdf007de35f07f873066de8a15d2c54c
sol <yongsung.kim@gmail.com>
2018-11-18 05:46:19 +0900

    ADD scim OBJECT

---
+++ ws-scim-core/src/main/java/com/wowsanta/scim/obj/JsonObject.java
@@ -0,0 +1,7 @@
+package com.wowsanta.scim.obj;
+
+import java.io.Serializable;
+
+public interface JsonObject extends Serializable {
+	 
+}
---
+++ ws-scim-core/src/main/java/com/wowsanta/scim/obj/Meta.java
@@ -0,0 +1,5 @@
+package com.wowsanta.scim.obj;
+
+public class Meta {
+
+}
