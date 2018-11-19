1569f001458712fbeb3378bbfdfdd8417ee71849
sol <yongsung.kim@gmail.com>
2018-11-20 02:55:03 +0900

    repository manager

--- ws-scim-core/src/main/java/com/wowsanta/scim/repository/SCIMRepositoryManager.java
+++ ws-scim-core/src/main/java/com/wowsanta/scim/repository/SCIMRepositoryManager.java
@@ -1,24 +1,12 @@
 package com.wowsanta.scim.repository;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.nio.charset.StandardCharsets;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.stream.JsonReader;
 import com.wowsanta.scim.resource.RepositoryManager;
 
 public class SCIMRepositoryManager {
 	private static SCIMRepositoryManager instance = null;
-	
-	private String repositoryClass;
-	private String repositoryConfig;
-	
 	private RepositoryManager repositoryManger;
 	
 	public static SCIMRepositoryManager getInstance() {
@@ -28,6 +16,25 @@ public class SCIMRepositoryManager {
 		return instance;
 	}
 
+	public void initialize(String repositoryClass, String repositoryConfig) {
+		try {
+			Method load_method = Class.forName(repositoryClass).getDeclaredMethod("load",String.class);
+			this.repositoryManger = (RepositoryManager)load_method.invoke(null,repositoryConfig);
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
 	public RepositoryManager getRepositoryManger() {
 		return repositoryManger;
 	}
@@ -36,45 +43,5 @@ public class SCIMRepositoryManager {
 		this.repositoryManger = repositoryManger;
 	}
 
-	public String getRepositoryClass() {
-		return repositoryClass;
-	}
 
-	public void setRepositoryClass(String repositoryClass) {
-		this.repositoryClass = repositoryClass;
-	}
-
-	public String getRepositoryConfig() {
-		return repositoryConfig;
-	}
-
-	public void setRepositoryConfig(String repositoryConfig) {
-		this.repositoryConfig = repositoryConfig;
-	}
-	
-	public void save(String file_name) throws IOException {
-		OutputStreamWriter writer = new OutputStreamWriter(
-				new FileOutputStream(
-						new File(file_name)),StandardCharsets.UTF_8);
-		
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		gson.toJson(this,writer);
-		writer.flush();
-		writer.close();
-	}
-	
-	public String toJson() {
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		return gson.toJson(this);
-	}
-	
-	public String toString() {
-		return toJson();
-	}
-	
-	public static void load(String file_name) throws FileNotFoundException {
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		JsonReader reader = new JsonReader(new FileReader(file_name));
-		instance =  gson.fromJson(reader,RepositoryManager.class);
-	}
 }
--- ws-scim-repo/src/test/java/com/wowsanta/scim/repo/RepositoryManagerTest.java
+++ ws-scim-repo/src/test/java/com/wowsanta/scim/repo/RepositoryManagerTest.java
@@ -16,19 +16,19 @@ public class RepositoryManagerTest {
 
 	private String conf_file = "../config/repository.json";
 	
-	@Test
+	//@Test
 	public void make() {
 		try {
-//			RDBRepository repository = new RDBRepository();
-//			DBCP dbcp = DBCP.load("../config/sqlite_dbcp.json");
-//			repository.initDBCP(dbcp, null);
-//			repository.save(conf_file);
+			RDBRepository repository = new RDBRepository();
+			DBCP dbcp = DBCP.load("../config/sqlite_dbcp.json");
+			repository.initDBCP(dbcp, null);
+			repository.save(conf_file);
 			
 			
-			SCIMRepositoryManager repo_mgr = SCIMRepositoryManager.getInstance();
-			repo_mgr.setRepositoryClass(RDBRepository.class.getCanonicalName());
-			repo_mgr.setRepositoryConfig("../config/sqlite_dbcp.json");
-			repo_mgr.save(conf_file);
+//			SCIMRepositoryManager repo_mgr = SCIMRepositoryManager.getInstance();
+//			repo_mgr.setRepositoryClass(RDBRepository.class.getCanonicalName());
+//			repo_mgr.setRepositoryConfig("../config/sqlite_dbcp.json");
+//			repo_mgr.save(conf_file);
 //			repo_mgr.setRepositoryManger(repository);
 			
 		} catch (IOException e) {
@@ -39,44 +39,16 @@ public class RepositoryManagerTest {
 	@Test
 	public void load() {
 		try {
+			
+			String repositoryClass = "com.wowsanta.scim.repo.rdb.RDBRepository";
 			SCIMRepositoryManager repo_mgr = SCIMRepositoryManager.getInstance();			
-			repo_mgr.load(conf_file);
+			repo_mgr.initialize(repositoryClass,this.conf_file);
 			
-			System.out.println(repo_mgr);
+			System.out.println(repo_mgr.getRepositoryManger());
 			
-		} catch (FileNotFoundException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
-//		try {
-//			//RDBRepository repository = RDBRepository.load(conf_file);
-//			Method load_method = Class.forName("com.wowsanta.scim.repo.rdb.RDBRepository").getDeclaredMethod("load",String.class);
-//			RepositoryManager repository = (RepositoryManager)load_method.invoke(null,this.conf_file);
-//			System.out.println("===repository===");
-//			System.out.println(repository);
-//			System.out.println("===repository===");
-////		} catch (FileNotFoundException e) {
-////			e.printStackTrace();
-//		} catch (IllegalAccessException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IllegalArgumentException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (InvocationTargetException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (NoSuchMethodException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (SecurityException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (ClassNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
 		
 	}
 }
